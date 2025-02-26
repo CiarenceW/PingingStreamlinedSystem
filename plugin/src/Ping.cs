136d631a1c05b759c3b6f8d824cdf49f49698911
CiarenceW <112902813+CiarenceW@users.noreply.github.com.>
2025-02-26 17:05:29 +0100

    Added ping sound & ping png, made the ping thing always look towards camera (actually it's backwards, because the forward on the plane is its backface, so it's not visible, lmao)

--- plugin/src/Ping.cs
+++ plugin/src/Ping.cs
@@ -4,16 +4,21 @@ namespace PingingStreamlinedSystem
 {
     public class Ping : MonoBehaviour
     {
+        static Vector2 size = Vector2.one;
+
         private void Awake()
         {
-            Destroy(this, PingingStreamlinedSystemPlugin.Options.pingDuration.Value);
+            Destroy(this.gameObject, PingingStreamlinedSystemPlugin.Options.pingDuration.Value);
+
+            var renderer = this.GetComponent<MeshRenderer>();
+            var mat = new Material(Shader.Find("Alloy/Unlit"));
+            renderer.material = mat;
+            renderer.material.mainTexture = PingingStreamlinedSystemPlugin.Instance.pingTexture;
         }
 
         private void Update()
         {
-            Rect rect = new Rect((Vector2)transform.position - Vector2.one, Vector2.one);
-
-            Gizmos.DrawGUITexture(rect, PingingStreamlinedSystemPlugin.Instance.pingTexture);
+            this.transform.LookAt((this.transform.position - Camera.main.transform.position) + this.transform.position, Vector3.up);
         }
     }
 }
--- plugin/src/PingingStreamlinedSystemPlugin.cs
+++ plugin/src/PingingStreamlinedSystemPlugin.cs
@@ -19,33 +19,68 @@ namespace PingingStreamlinedSystem
     {
         internal static PingingStreamlinedSystemPlugin Instance { get; set; }
 
+        private static bool testing;
+
         internal Texture2D pingTexture;
+
+        internal AudioEvent pingAudio;
 
         private Stack<LaserHitInfo> recentlyPointedObjects = new Stack<LaserHitInfo>(16);
 
         private void Awake()
         {
+            Logger = base.Logger;
+
             Instance = this;
 
-            StartCoroutine(LoadPingTexture(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "icon.png"));
+            StartCoroutine(LoadPingTexture(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/ping.png"));
+
+            StartCoroutine(LoadPingAudio(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/ping.ogg"));
 
             Options.InitialiseAndBind(Config);
-
-            Logger = base.Logger;
             
+            Harmony.CreateAndPatchAll(this.GetType());
+
             // Your plugin's ID, Name, and Version are available here.
             Logger.LogMessage($"{Id}: {Name} version {Version} loaded!");
         }
 
-        private IEnumerator LoadPingTexture(string path)
+        private IEnumerator LoadPingAudio(string path)
         {
+            Logger.LogMessage(path);
+
             if (!File.Exists(path))
                 yield break;
 
-            pingTexture = new Texture2D(256, 256, TextureFormat.DXT5, false);
             WWW www = new WWW("file://" + path);
-            yield return www;
+            while (!www.isDone)
+            {
+                Logger.LogMessage($"Loading Ping audio: {www.progress}");
+                yield return null;
+            }
+
+            pingAudio = new AudioEvent();
+            pingAudio.Clips.Add(www.GetAudioClip(true, false, AudioType.OGGVORBIS));
+            yield break;
+        }
+
+        private IEnumerator LoadPingTexture(string path)
+        {
+            Logger.LogMessage(path);
+
+            if (!File.Exists(path))
+                yield break;
+
+            pingTexture = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+            WWW www = new WWW("file://" + path);
+            while (!www.isDone)
+            {
+                Logger.LogMessage($"Loading Ping texture: {www.progress}");
+                yield return null;
+            }
+
             www.LoadImageIntoTexture(pingTexture);
+            yield break;
         }
 
         [HarmonyPatch(typeof(FVRViveHand), nameof(FVRViveHand.Update))]
@@ -62,12 +97,17 @@ namespace PingingStreamlinedSystem
                 isShootingLaser = __instance.Input.TouchpadDown;
             }
 
+            if (testing)
+            {
+                isShootingLaser = Input.GetMouseButton(2);
+            }
+
             if (isShootingLaser)
             {
                 Instance.ShootLaser(__instance);
             }
 
-            if (isShootingLaser && __instance.Input.TriggerPressed)
+            if (isShootingLaser && (__instance.Input.TriggerPressed || Input.GetMouseButtonDown(0) && testing))
             {
                 Instance.Ping();
             }
@@ -75,12 +115,31 @@ namespace PingingStreamlinedSystem
 
         private void ShootLaser(FVRViveHand hand)
         {
-            if (Physics.Raycast(hand.Input.OneEuroPointingPos, hand.Input.OneEuroPointRotation * Vector3.forward, out var hit, Mathf.Infinity, hand.GrabLaserMask, QueryTriggerInteraction.Collide))
+            if (testing && hand.IsThisTheRightHand)
             {
-                recentlyPointedObjects.Push(new LaserHitInfo() {
-                    hitObject = hit.collider.transform,
-                    hitPos = hit.point
-                });
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Gizmos.color = Color.red;
+                Gizmos.DrawRay(ray);
+
+                if (Physics.Raycast(ray, out var shit, Mathf.Infinity, hand.GrabLaserMask, QueryTriggerInteraction.Collide))
+                {
+                    recentlyPointedObjects.Push(new LaserHitInfo()
+                    {
+                        hitObject = shit.collider.transform,
+                        hitPos = shit.point
+                    });
+                }
+            }
+            else
+            {
+                if (Physics.Raycast(hand.Input.OneEuroPointingPos, hand.Input.OneEuroPointRotation * Vector3.forward, out var hit, Mathf.Infinity, hand.GrabLaserMask, QueryTriggerInteraction.Collide))
+                {
+                    recentlyPointedObjects.Push(new LaserHitInfo()
+                    {
+                        hitObject = hit.collider.transform,
+                        hitPos = hit.point
+                    });
+                }
             }
         }
 
@@ -88,18 +147,28 @@ namespace PingingStreamlinedSystem
         {
             var lastHitInfo = recentlyPointedObjects.Pop();
 
-            CreatePingObject(lastHitInfo.hitPos, lastHitInfo.hitObject);
+            var pingGo = CreatePingObject(lastHitInfo.hitPos, lastHitInfo.hitObject);
+            SM.PlayGenericSound(pingAudio, pingGo.transform.position);
         }
 
         private GameObject CreatePingObject(Vector3 pos, Transform parent = null)
         {
-            var pingGo = new GameObject("ping");
+            var pingGo = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            {
+                name = "ping";
+            }
 
             pingGo.AddComponent<Ping>();
 
-            pingGo.transform.position = pos;
-
-            pingGo.transform.parent = parent;
+            if (parent == null)
+            {
+                pingGo.transform.position = pos;
+            }
+            else
+            {
+                pingGo.transform.parent = parent;
+                pingGo.transform.localPosition = Vector3.zero;
+            }
 
             return pingGo;
         }
